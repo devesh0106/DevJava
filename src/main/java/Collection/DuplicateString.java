5cacb0de52c63cd1b55f9a4fecfeeb09c7f7ac29
Devesh Anand <deveshlive2912@gmail.com>
2023-08-29 20:47:42 +0530

    adding new

--- src/main/java/Collection/DuplicateString.java
+++ src/main/java/Collection/DuplicateString.java
@@ -5,13 +5,12 @@ import java.util.Set;
 
 public class DuplicateString {
     public static void main(String[] args) {
-        String str =" Devesh Anand";
+        String str ="Devesh";
         char[] string = str.toCharArray();
         Set<Character> ch = new HashSet<>();
         for( char c : string){
             if(ch.add(c)==false){
                 System.out.println(c);
-                //
             }
         }
     }
